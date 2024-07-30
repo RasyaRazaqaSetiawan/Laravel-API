90516a3eb3959c75dc41a6bbeee06fa6b72e673c
Rasya <ratagarox@gmail.com>
2024-07-30 08:48:48 +0700

    CRUD KLUB

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LigaController;
+use App\Http\Controllers\Api\KlubController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('liga', [Ligacontroller::class, 'index']);
-Route::post('liga', [Ligacontroller::class, 'store']);
-Route::get('liga/{id}', [Ligacontroller::class, 'show']);
-Route::put('liga/{id}', [Ligacontroller::class, 'update']);
-Route::delete('liga/{id}', [Ligacontroller::class, 'destroy']);
+// Route Manual
+// Route::get('liga', [Ligacontroller::class, 'index']);
+// Route::post('liga', [Ligacontroller::class, 'store']);
+// Route::get('liga/{id}', [Ligacontroller::class, 'show']);
+// Route::put('liga/{id}', [Ligacontroller::class, 'update']);
+// Route::delete('liga/{id}', [Ligacontroller::class, 'destroy']);
+
+//Route Matic
+Route::resource('liga', LigaController::class)->except(['edit', 'create']);
+Route::resource('klub', KlubController::class)->except(['edit', 'create']);
